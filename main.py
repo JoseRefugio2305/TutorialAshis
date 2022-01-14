88e208a1a136c22b4fb94adb2b7866ebc7f717ed
JoseRefugio2305 <joserefugioriveramendoza@gmail.com>
2022-01-14 13:55:44 -0600

    V1

---
+++ main.py
@@ -0,0 +1,36 @@
+from logging import debug
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='secret'
+socketiocon = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketiocon.on('message')
+def handleMessage(msg, id):
+    print("El mensaje enviado es: "+msg+" enviado por el usuario: "+str(id))
+    send(msg, to=id)
+
+@socketiocon.on('join')
+def on_join(data):
+    username = 'jose'
+    room = data["room"]
+    print(data["room"])
+    join_room(room)
+    send(username + ' has entered the room.', to=room)
+
+@socketiocon.on('leave')
+def leave(room):
+    username='jose'
+    print("leave")
+    leave_room(room['room'])
+    send(username+" abandono la habitacion", to=room['room'])
+
+if __name__=='__main__':
+    #socketiocon.run(app, debug=True)
+    socketiocon.run(app)
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Python                    3.9.1
+Flask                     2.0.0
+Werkzeug                  2.0.0
+Flask-MySQLdb             0.2.0
+Flask-Session             0.4.0
+Flask-SocketIO            5.1.1 (se utiliza si se desbloquean lineas bloqueadas de mensajes de SocketIO)
+Jinja2                    3.0.0 (se instala con flask)
+HTML    
+Werkzeug                  2.0.0
+Mysql                     5.7
